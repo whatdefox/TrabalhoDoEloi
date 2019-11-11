e6f5d01765b60e0c309ed4ea8b9108cff2d6a5ea
whatdefox <36849609+whatdefox@users.noreply.github.com>
2019-11-10 22:27:27 -0300

    Add files via upload

---
+++ ELOI.COM.BR/fisica.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Trabalho de fisica">
+  <meta name="author" content="RenatoBuzinaro">
+
+  <title>Apresentação Física</title>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="js/jquery-3.3.1.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Estilo próprio desse site -->
+  <link href="css/customizado.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <header>
+    <!-- Menu Fixo -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#"><b>Trabalho de Física 3 Trimestre</b></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+
+          </li>
+        </ul>
+        <span class="navbar-text text-center">
+          Renato Galvão Buzinaro
+        </span>
+      </div>
+    </nav>
+
+  </header>
+
+
+  <!-- Conteúdo -->
+  <main  class="container ">
+
+
+
+    <div class="row">
+      <div class="col-md-5 mx-auto">
+        <h1 class="mt-4 text-center">Eletromagnetismo</h1>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="row pt-3 ">
+    <div class="col-md-6 mx-auto py-3">
+
+      <p class="text-justify"><b>Uma bobina chata é formada de 40 espiras circulares,
+        de raio 8,0 cm. A intensidade da corrente que percorre a
+        bobina, quando a intensidade do vetor campo magnético
+        no centro da bobina é 6,0 ⋅ 10–4
+        T, é de(μ = 4 π ⋅ 10–7
+        T).</b></p>
+      </div>
+    </div>
+
+
+    <div class="row">
+      <div class="col-sm-3 mx-auto py-4 min-vh-100">
+        <a href="tutorialpg2.html" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Aprenda a utilizar</a>
+      </div>
+    </div>
+
+
+
+  </main>
+
+
+  <!-- Rodapé -->
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted"><a href="https://github.com/whatdefox/TrabalhoCheatEngine"> Verifique o código aqui.</a></span>
+    </div>
+  </footer>
+
+</body>
+</html>
